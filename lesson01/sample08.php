ba2c39c1c12ff9c01496a425c870fbd5b2d67dac
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-01-04 23:55:31 +0900

    temporary commit

---
+++ lesson01/sample08.php
@@ -0,0 +1,4 @@
+<?php
+for ($i=1; $i<366; $i=$i+1):
+    echo $i.'<br>';
+endfor;
---
+++ lesson01/sample09.php
@@ -0,0 +1,9 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
+
+for($i=0; $i<366; $i++):
+    $time = strtotime("+$i day");
+    $day = date('n/j(D)', $time);
+    echo $day.'<br>';
+endfor;
+?>
